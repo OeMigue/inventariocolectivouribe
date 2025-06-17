8c4c6c984a5abfb38faaf57f9aa6e71f7431eb40
OeMigue <miguelcardonahenao23022008@gmail.com>
2025-06-17 15:12:53 -0500

    Feat Se crea el menú de opciones y se empieza el ciclo

---
+++ src/main/java/org/example/Miguel/Inventario.java
@@ -0,0 +1,12 @@
+package org.example.Miguel;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class Inventario
+{
+    //1. PROCESO --> MENÚ DE OPCIONES
+    Integer OPCION=0;
+
+}
